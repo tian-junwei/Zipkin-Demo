59e536897f5d0df24f87840069ae6e67fd1b1ad7
tianjunwei <924862077@qq.com>
2018-05-09 14:44:33 +0800

    修改zipkin版本

--- tjw.zipkin/src/main/java/com/tianjunwei/Application.java
+++ tjw.zipkin/src/main/java/com/tianjunwei/Application.java
@@ -1,9 +1,11 @@
 package com.tianjunwei;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
