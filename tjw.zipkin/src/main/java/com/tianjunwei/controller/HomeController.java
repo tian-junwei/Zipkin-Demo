b7cda2d842e3c3979546224d11147281ad1c8897
tianjunwei <924862077@qq.com>
2018-05-11 18:40:07 +0800

    添加示例代码

--- tjw.zipkin/src/main/java/com/tianjunwei/controller/HomeController.java
+++ tjw.zipkin/src/main/java/com/tianjunwei/controller/HomeController.java
@@ -17,51 +17,17 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/")
 public class HomeController {
 
-	static ThreadPoolExecutor  executor = new ThreadPoolExecutor(1, 1, 200, TimeUnit.MILLISECONDS,
-            new ArrayBlockingQueue<Runnable>(5));
-	/*static {
-		executor.execute(new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				System.err.println("hello"+Thread.currentThread().hashCode());
-				
-			}
-		}));
-	}*/
+	
 	
 	@Autowired 
 	RestTemplate restTemplate;
 	
-	String data = "";
+
 
     @RequestMapping("start")
     public String start(HttpServletRequest request1,HttpServletResponse response1) throws InterruptedException, IOException {
     	
-    	
-    	
-    	Thread thread1 = new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				
-				System.err.println(Thread.currentThread().hashCode());
-			}
-		});
-    	executor.execute(thread1);
-    	
-    	Thread.sleep(1000);
-    	
-    	Thread thread = new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				System.err.println(Thread.currentThread().hashCode());
-				data = restTemplate.getForObject("http://localhost:9090/foo", String.class);
-			}
-		});
-    	executor.execute(thread);
-    	Thread.sleep(10000);
+    	String data = restTemplate.getForObject("http://localhost:9090/foo", String.class);
     	
     	return data;
     }
